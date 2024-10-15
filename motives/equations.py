1ef863b9ca62179fbd08268278ea3c819164d3f4
David Alfaya <145925630+david-alfaya@users.noreply.github.com>
2024-10-15 12:30:24 +0200

    Update descriptions of equations.py

--- motives/equations.py
+++ motives/equations.py
@@ -8,12 +8,16 @@ from .grothendieck_motives.curves.moduli_motives import TwistedHiggsModuli
 def compare_equation(
     g: int, p: int, r: int, filename: str, time: bool = True, *, verbose: int = 0
 ) -> bool:
-    """Check that the motives calculated using both derivations are equal.
+    """Check that the equations for the motive of the moduli space of L-twisted Higgs bundles
+    of rank r over a genus g curve calculated using the equation derived from the Bialynicki-Birula
+    decomposition of the moduli from [Alfaya, Oliveira 2024, Corollary 8.1] and the conjectural
+    equation obtained in [Mozgovoy 2012, Conjecture 3] as a solution to the ADHM equation
+    (see [Chuang, Diaconescu, Pan 2011]) are equal.
 
     Args:
         g: The genus of the curve.
-        p: The number of points on the curve.
-        r: The rank of the formula to compare.
+        p: Positive integer such that deg(L)=2g-2+p.
+        r: The rank of the the underlying vector bundles. Currently only implemented for r<=3.
         filename: Name of the file where to save the polynomial once computed.
         time: Whether to print the time it took to compute the polynomials.
         verbose: How much to print when deriving the formula. 0 is nothing at all,
@@ -25,7 +29,7 @@ def compare_equation(
     start = perf_counter()
 
     gc = GrothendieckRingContext()
-    cur = Curve("x", g=g)
+    cur = Curve("X", g=g)
 
     # Compute the motive of rank r using ADHM derivation
     moz = TwistedHiggsModuli(x=cur, p=p, r=r, method="ADHM")
