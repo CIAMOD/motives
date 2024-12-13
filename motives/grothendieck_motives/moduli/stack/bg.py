3575ca913e6a7cc1dc71b29a963b5fd12dfedc4f
winoo19 <danielsanchezs1903@gmail.com>
2024-12-13 13:35:48 +1300

    [class] Completed classes bung and bun

--- motives/grothendieck_motives/moduli/stack/bg.py
+++ motives/grothendieck_motives/moduli/stack/bg.py
@@ -1,4 +1,205 @@
-class BG:
-    def __init__(self):
-        # TODO: Implement this class
-        raise NotImplementedError("This class is not implemented yet.")
+from ...motive import Motive
+from ...groups.g import G
+from ...curves.curve import Curve
+from ...lefschetz import Lefschetz
+from ....core.operand import Operand
+
+import sympy as sp
+from multipledispatch import dispatch
+
+
+class BunG(Motive, sp.AtomicExpr):
+    """
+    Represents the moduli stack of vector bundles on a curve in an expression tree.
+
+    The `BunG` is a motive associated with a `Curve` object and a general group `G`.
+    It supports Adams and Lambda operations, generating functions, and interacts with other motives.
+
+    Attributes:
+    -----------
+    curve : Curve
+        The curve for which this BunG is defined.
+    group : G
+        The general group associated with this BunG.
+    lef : Lefschetz
+        The Lefschetz motive.
+    """
+
+    def __new__(cls, curve: Curve, group: G, *args, **kwargs):
+        """
+        Creates a new instance of the `BunG` class.
+
+        Args:
+        -----
+        curve : Curve
+            The curve for which to create the BunG.
+        group : G
+            The general group.
+
+        Returns:
+        --------
+        BunG
+            A new instance of the `BunG` class.
+        """
+        new_bun = sp.AtomicExpr.__new__(cls)
+        new_bun._assumptions["commutative"] = True
+        return new_bun
+
+    def __init__(self, curve: Curve, group: G, *args, **kwargs) -> None:
+        """
+        Initializes a `BunG` instance.
+
+        Args:
+        -----
+        curve : Curve
+            The curve for which this BunG is defined.
+        group : G
+            The general group.
+        """
+        self.curve: Curve = curve
+        self.group: G = group
+        self.lef: Lefschetz = Lefschetz()
+
+        self._et_repr: sp.Expr = self.lef ** (
+            (self.curve.g - 1) * sum(self.group.alfas)
+        ) * sp.Mul(*[self.curve.Z(self.lef ** (-alfa)) for alfa in self.group.alfas])
+
+        self._lambda_vars: dict[int, sp.Expr] = {}
+        self._adams_vars: dict[int, sp.Expr] = {}
+
+    def __repr__(self) -> str:
+        """
+        Returns the string representation of the BunG.
+
+        Returns:
+        --------
+        str
+            A string representation in the form of "BunG_{curve}_{group}".
+        """
+        return f"BunG_{self.curve}_{self.group}"
+
+    def _hashable_content(self) -> tuple:
+        """
+        Returns the hashable content of the BunG.
+
+        Returns:
+        --------
+        tuple
+            A tuple containing the curve and the general linear group.
+        """
+        return (self.curve, self.group)
+
+    @property
+    def free_symbols(self) -> set:
+        """
+        Returns the set of free symbols in the BunG.
+
+        Returns:
+        --------
+        set
+            The set of free symbols in the BunG.
+        """
+        return self._et_repr.free_symbols
+
+    def get_adams_var(self, i: int) -> sp.Expr:
+        """
+        Returns the BunG with an Adams operation applied to it.
+
+        Args:
+        -----
+        i : int
+            The degree of the Adams operator.
+
+        Returns:
+        --------
+            The BunG with the Adams operator applied.
+        """
+        if i not in self._adams_vars:
+            self._adams_vars[i] = self._et_repr.to_adams(i)
+        return self._adams_vars[i]
+
+    def get_lambda_var(self, i: int) -> sp.Expr:
+        """
+        Returns the BunG with a Lambda operation applied to it.
+
+        Args:
+        -----
+        i : int
+            The degree of the Lambda operator.
+
+        Returns:
+        --------
+        sp.Expr
+            The BunG with the Lambda operator applied.
+        """
+
+        if i not in self._lambda_vars:
+            self._lambda_vars[i] = self._et_repr.to_lambda(i)
+        return self._lambda_vars[i]
+
+    def _to_adams(self, degree: int, ph: sp.Expr) -> sp.Expr:
+        """
+        Applies the Adams operator to any instances of this BunG in the expression.
+
+        Args:
+        -----
+        degree : int
+            The degree of the Adams operator to apply.
+        ph : sp.Expr
+            The polynomial in which to apply the Adams operator.
+
+        Returns:
+        --------
+        sp.Expr
+            The polynomial with the Adams operator applied.
+
+        Raises:
+        -------
+        Exception
+            Always raised as BunGs should be decomposed into their components.
+        """
+        raise Exception(
+            f"There is a BunG in the expression {ph}. "
+            "It should have been converted to its components."
+        )
+
+    def _subs_adams(self, ph: sp.Expr) -> sp.Expr:
+        """
+        Substitutes Adams variables in the polynomial with equivalent Lambda polynomials.
+
+        Args:
+        -----
+        ph : sp.Expr
+            The polynomial in which to substitute the Adams variables.
+
+        Returns:
+        --------
+        sp.Expr
+            The polynomial with Adams variables substituted by Lambda polynomials.
+
+        Raises:
+        -------
+        Exception
+            Always raised as BunGs should be decomposed into their components.
+        """
+        raise Exception(
+            f"There is a BunG in the expression {ph}. "
+            "It should have been converted to its components."
+        )
+
+    @dispatch(set)
+    def _to_adams(self, operands: set[Operand]) -> sp.Expr:
+        """
+        Converts this BunG into an equivalent Adams polynomial.
+
+        Args:
+        -----
+        operands : set[Operand]
+            The set of all operands in the expression tree.
+
+        Returns:
+        --------
+        sp.Expr
+            The Adams polynomial equivalent to this BunG.
+        """
+        return self._et_repr.to_adams()
--- motives/grothendieck_motives/moduli/stack/bun.py
+++ motives/grothendieck_motives/moduli/stack/bun.py
@@ -1,6 +1,52 @@
-class Bun:
-    def __init__(self, r,d):
-        self.r = r
-        self.d = d
-        # TODO: Implement this class
-        raise NotImplementedError("This class is not implemented yet.")
+from bg import BunG
+from ...curves.curve import Curve
+from ...groups.gl import GL
+
+
+class Bun(BunG):
+    """
+    Represents the moduli stack of vector bundles on a curve with a general linear group GL(n).
+
+    The `Bun` is a specialized version of `BunG` where the group is specifically GL(n).
+    It supports Adams and Lambda operations, generating functions, and interacts with other motives.
+
+    Attributes:
+    -----------
+    curve : Curve
+        The curve for which this Bun is defined.
+    n : int
+        The dimension of the general linear group GL(n).
+    """
+
+    n: int
+
+    def __new__(cls, curve: Curve, n: int, *args, **kwargs):
+        """
+        Creates a new instance of the `Bun` class.
+
+        Args:
+        -----
+        curve : Curve
+            The curve for which to create the Bun.
+        n: int
+            The dimension of the general linear group GL(n).
+
+        Returns:
+        --------
+        Bun
+            A new instance of the `Bun` class.
+        """
+        new_bun = BunG.__new__(cls, curve, GL(n))
+        new_bun.n = n
+        return new_bun
+
+    def __repr__(self) -> str:
+        """
+        Returns the string representation of the Bun.
+
+        Returns:
+        --------
+        str
+            A string representation in the form of "Bun_{curve}_{n}".
+        """
+        return f"Bun_{self.curve}_{self.n}"
