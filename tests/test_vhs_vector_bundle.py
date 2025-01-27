4b3fd6d86e35d0508c2644481c128fa510f3395d
winoo19 <danielsanchezs1903@gmail.com>
2025-01-27 11:41:28 +1100

    [Test] Created test as_symbol and fixed test vhs

--- tests/test_vhs_vector_bundle.py
+++ tests/test_vhs_vector_bundle.py
@@ -7,17 +7,31 @@ import math
     "g,p,rks",
     (
         (g, p, rks)
-        for g in range(2, 6)
-        for p in range(2, 6)
-        for rks in [(1, 1), (1, 2), (2, 1), (1, 1, 1)]
+        for g in range(1, 4)
+        for p in range(1, 4)
+        for rks in [(1, 1), (1, 2), (1, 1, 1)]
     ),
 )
 def test_vhs(g, p, rks):
     dl = 2 * g - 2 + p
-    end_range = (
-        math.floor(dl / 2 + 1 / 3) + 1
+    vhs_degs = (
+        ((i, -i + 1) for i in range(1, math.floor(dl / 2 + 1 / 3) + 1))
         if rks == (1, 2)
-        else ((dl + 1) // 2 + 1 if rks == (1, 1) else math.floor(dl / 2 + 2 / 3) + 1)
+        else (
+            ((i, -i + 1) for i in range(1, (dl + 1) // 2 + 1))
+            if rks == (1, 1)
+            else (
+                (i, j, -i - j + 1)
+                for i in range(1, dl + 1)
+                for j in range(max(-dl + i, 1 - i), math.floor((dl - i + 1) / 2) + 1)
+            )
+        )
+    )
+
+    l_exp = (
+        3 * dl + 2 - 2 * g
+        if rks == (1, 1)
+        else (7 * dl + 5 - 5 * g if rks == (1, 2) else 6 * dl + 3 - 3 * g)
     )
 
     cur = Curve("c", g)
@@ -29,13 +43,16 @@ def test_vhs(g, p, rks):
     vhs_thm = thm.vhs[rks]
 
     vhs_real = 0
-    for i in range(1, end_range):
-        vhs = VHS(cur, rks, (i, -i + 1), p)._et_repr
+    for degs in vhs_degs:
+        vhs = VHS(cur, rks, degs, p)._et_repr
         vhs_real += vhs
 
-    vhs_real = vhs_real * lef ** (3 * dl + 2 - 2 * g)
+    vhs_real = vhs_real * lef**l_exp
 
-    assert (vhs_thm - vhs_real).to_adams(as_symbol=False).simplify() == 0
+    vhs_thm_simplified = vhs_thm.to_adams(as_symbol=False).cancel().expand()
+    vhs_real_simplified = vhs_real.to_adams(as_symbol=False).cancel().expand()
+
+    assert vhs_thm_simplified == vhs_real_simplified
 
 
 @pytest.mark.parametrize("g,r", ((g, r) for g in range(2, 6) for r in [2, 3]))
